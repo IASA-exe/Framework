ea8c320167f76220fb11eeac48b9db5851e5989f
승빈 오 <sboh1214@gmail.com>
2018-11-05 20:14:44 +0900

    C# 파일 추가

---
+++ Filter.cs
@@ -0,0 +1,21 @@
+
+
+namespace Photo_Retouch.UWP
+{
+    public partial class MainPage
+    {
+        public Pixel[,] ReverseFilter(Pixel[,] InArr, Pixel[,] OutArr, int row, int col)
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    OutArr[i, j].r = InArr[i, j].g;
+                    OutArr[i, j].g = InArr[i, j].b;
+                    OutArr[i, j].b = InArr[i, j].r;
+                }
+            }
+            return OutArr;
+        }
+    }
+}
